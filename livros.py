184e827a19c8fc6b9d8ebfc8c9105b855ff0a015
André Luis <condelectarisibi@gmail.com>
2024-06-26 21:13:23 -0300

    Second step with Selenium

--- livros.py
+++ livros.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 #!-*-coding:utf-8-*-
 
+import time
+
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+
 
 driver = webdriver.Firefox()
 driver.get("https://www.estantevirtual.com.br/")
-print(driver.title)
-driver.quit()
+elem = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/header/div[2]/div/div[2]/form/div[2]/input")
+elem.send_keys("Meridiano de Sangue")
+time.sleep(1.5)
+elem2 = driver.find_element(By.XPATH, "/html/body/div[2]/div/div/header/div[2]/div/div[2]/form/div[2]/button")
+elem2.click()
+
